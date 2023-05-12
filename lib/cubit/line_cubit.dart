cf58236abbaf4c59b56b3f81f5a06989d7f1f2d8
mainjzb <mainjzb@live.com>
2023-05-12 19:01:45 +0800

    需要更多的学习bloc

--- lib/cubit/line_cubit.dart
+++ lib/cubit/line_cubit.dart
@@ -8,6 +8,9 @@ part 'line_state.dart';
 class LineCubit extends Cubit<LineState> {
   LineCubit() : super(const LineState(line: Line.empty));
 
+  // TODO: 把state全都放到这个类里，让类来控制。
+  // https://bloclibrary.dev/#/fluttertodostutorial
+
   void selectSelf(Line line) {
     emit(LineState(status: LineStatus.editing, line: line.copyWith()));
   }
@@ -17,8 +20,7 @@ class LineCubit extends Cubit<LineState> {
   }
 
   void selectNext(Line line) {
-    emit(LineState(status: LineStatus.initial, line: line.copyWith()));
-
+    complete(line);
     // 下一个的状态没法获取
     final newLine = line.copyWith(index: line.index + 1);
     emit(LineState(status: LineStatus.editing, line: newLine));
